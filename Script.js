46a243d13236329320d18492aba6710b0f86c724
Alex <aleksei.shevlyakoff@yandex.ru>
2021-01-15 13:58:32 +0300

    Это наш второй комит

---
+++ Script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let a = 5;
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log('2' === 2);
+
+let isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose);
